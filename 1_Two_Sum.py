4c1eb3529f3b5fad2f6ebbf8cba66bc065ff7f66
achalnm <achalnm02@gmail.com>
2026-03-16 21:26:28 +0000

    Two Sum

---
+++ 1_Two_Sum.py
@@ -0,0 +1,26 @@
+'''
+nums = [2,7,11,15]
+target = 18 
+
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print("Target found")
+            print (i,j)
+
+'''    
+
+nums = [2,7,11,15]
+target = 18 
+hashmap = {}
+    
+for i, num in enumerate(nums):
+    complement = target - num
+        
+    if complement in hashmap:
+        print ( [hashmap[complement], i] )
+        
+    hashmap[num] = i
+
+        
+    
