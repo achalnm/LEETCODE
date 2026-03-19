716e441482b01c0c4efbfde63fb90bee1e26f9f6
achalnm <achalnm02@gmail.com>
2026-03-19 01:33:37 +0000

    fixed stuff

--- 31_Next_Permutation.py
+++ 31_Next_Permutation.py
@@ -1,17 +1,31 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums):
         n = len(nums)
+        
+        # Step 1: find first decreasing element from right
         i = n - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
+        
+        # Step 2: if found, swap with just bigger element on right
         if i >= 0:
             j = n - 1
             while nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
+        
+        # Step 3: reverse the rest
         left = i + 1
         right = n - 1
         while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
+
+nums = [1, 2, 3]
+print("Original:", nums)
+
+sol = Solution()
+sol.nextPermutation(nums)
+
+print("Next permutation:", nums)
