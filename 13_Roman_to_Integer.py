e31b4d7b93be4d2a9b4cc546a99eab3cfd14d39a
achalnm <achalnm02@gmail.com>
2026-03-25 21:27:35 +0000

    Roman3

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -1,4 +1,4 @@
-s = "III"
+s = "IV"
 
 roman = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
@@ -12,4 +12,4 @@ for i in range(len(s)):
     else:
          total = total + roman[s[i]]
     
-print (total)
+print(total)
